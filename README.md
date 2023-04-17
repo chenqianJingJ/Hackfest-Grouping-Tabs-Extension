25bf844a2d4c657b071ad9757db74acedf32a292
chenqianJingJ <cjing@diligent.com>
2023-04-17 16:06:25 -0700

    Worked!

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hackfest-Grouping-Tabs-Extension
+
+## Prerequisite
+Please upgrade your chrome to the latest version, so chrome.tabGroups Api is enabled.
--- background.js
+++ background.js
@@ -1,37 +1,62 @@
-// import { Configuration, OpenAIApi } from "openai";
-// const configuration = new Configuration({
-//     organization: "org-VdCe6pgSqAPHvrjP0f8rJW97",
-//     apiKey: process.env.OPENAI_API_KEY,
-// });
-// const openai = new OpenAIApi(configuration);
-const openai = require('openai');
-// Set up your OpenAI API key
-const apiKey = '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';
-openai.apiKey = apiKey;
-// Call the function to get all current tabs
-let tabsUrl = getAllTabsUrl();
-// Define your prompt
-const prompt = `Please classify and group related urls ${tabsUrl}`;
-// Set the parameters for the text generation
-const parameters = {
-  prompt: prompt,
-  max_tokens: 100
-};
-// Call the OpenAI API to generate text
-openai.Completions.create(parameters, (error, response) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    console.log('Generated text:', response.choices[0].text);
+groupTabs();
+
+async function groupTabs() {
+  try {
+    const tabsCollection = await getAllTabsUrl();
+    const tabsUrl = Object.keys(tabsCollection);
+    console.log(tabsCollection);
+    const apiKey = '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';
+    const response = await fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        "model": "text-davinci-003",
+        "prompt": `Please classify and group related urls ${tabsUrl}. Please response a stringified json format: name of the classification, array of following related urls. Example: {"AWS Related": ["https...", "https://"]}`,
+        "max_tokens": 3000,
+      })
+    });
+    const responseData = await response.json();
+    const responseText = JSON.parse(responseData.choices[0].text);
+    console.log(responseText);
+
+    for (let key in responseText) {
+      console.log(key);
+      console.log(typeof key);
+      let tabIds = [];
+      let urlList = responseText[key];
+      console.log(urlList);
+      for (let index in urlList) {
+        let tabId = tabsCollection[urlList[index]];
+        tabIds.push(tabId);
+      };
+      const groupId = await chrome.tabs.group({ tabIds });
+      console.log(groupId);
+      await chrome.tabGroups.update(groupId, { title: key });
+    }
+  } catch (error) {
+    console.error(error);
   }
-});
+}
+
 /*
 Define a function to get all current tabs in current window
 */
 function getAllTabsUrl() {
-  // Use chrome.tabs.query() to get all tabs
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    let tabsUrl = tabs.map(tab => tab.url);
-    console.log(tabsUrl);
+  // Create a new Promise
+  return new Promise((resolve, reject) => {
+    // Use chrome.tabs.query() to get all tabs
+    chrome.tabs.query({ currentWindow: true }, function (tabs) {
+      let tabsCollection = {};
+      tabs.forEach(function (tab) {
+        tabsCollection[tab.url] = tab.id;
+      });
+      // Resolve the Promise with the result
+      resolve(tabsCollection);
+    });
   });
 };
+
+
